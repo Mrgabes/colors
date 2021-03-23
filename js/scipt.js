3a6b558fc2c9dfb7659d73b20d7ba5b3138c7e9e
Isaac Moreno <isaacmoreno@Isaacs-Mac-mini.local>
2021-03-23 13:15:10 -0700

    BUGFIX fix syntax error to add slategray button
    Co-authored-by: Gabriel <mrgabes72@gmail.com>

--- js/scipt.js
+++ js/scipt.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
   $("button#green").click(function() {
-     $("body").removeClass();
+    $("body").removeClass();
     $("body").addClass("green-background");
   });
 
   $("button#yellow").click(function() {
-     $("body").removeClass();
+    $("body").removeClass();
     $("body").addClass("yellow-background");
   });
 
   $("button#red").click(function() {
-    // $("body").removeClass();
+    $("body").removeClass();
     $("body").addClass("red-background");
   });
+
+  $("button#slategray-background").click(function () {
+    $("body").removeClass();
+    $("body").addClass("slategray-background");
+  });
 });
+
